c6eb9b4b988d06dd854c9b8fb782bc96b30271af
Taras Kharuk <taras.kharuk@gmail.com>
2022-03-25 10:37:59 +0200

    remove redundant files

--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-export const formatDaysAgo = (value) => {
-  const date = new Date(value);
-  const deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
-  const formatter = new Intl.RelativeTimeFormat("en", { style: "narrow" });
-  const formatted = formatter.format(Math.round(deltaDays), "days");
-
-  return formatted;
-};
