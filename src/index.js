656b81a68c99e51c2e8537d1e3a56299c965da74
Taras Kharuk <taras.kharuk@gmail.com>
2022-03-25 11:15:44 +0200

    cleanups

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 import { queryClient } from "api";
 
@@ -11,8 +10,6 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-
-      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
